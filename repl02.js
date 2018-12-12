af6af50c738b6d1e1bf09c26bcc4e65ddad238da
Nolan Kovacik <noltron000@gmail.com>
2018-12-11 16:27:45 -0800

    phase 3 - completing template strings etc

--- repl02.js
+++ repl02.js
@@ -1,24 +1,19 @@
-// note: the result from the assignment was an impossible outcome.
-// the result has instead been shifted at position (3,3) to 'X' to fix this.
-
-const game = [
-	'o', 'o', 'x',
-	'o', 'x', 'o',
-	'x', 'o', 'x'
-]
-
-const draw = (game) => {
-	let row = ''
-	for (let index = 0; index < 10; index++) {
-		row += game[index]
-		if (index % 3 == 2) {
-			console.log(row);
-			row = ''
-		}
-		else {
-			row += ' '
-		}
+class Person {
+	constructor(name, age, id) {
+		this.name = name;
+		this.age = age;
+		this.id = id;
+	}
+	whoIs() {
+		const info
+			= '===INFORMATION===\n'
+			+ `Name: ${this.name}\n`
+			+ `Age: ${this.age}\n`
+			+ `ID: ${this.id}\n`
+		return info
 	}
 }
 
-draw(game)
+const Wegman = new Person('Wegman', 33, 9348576)
+
+console.log(Wegman.whoIs())
