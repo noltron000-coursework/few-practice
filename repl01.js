ed48d132cf6c333b763f0cecd21d46a106060d89
Nolan Kovacik <noltron000@gmail.com>
2018-12-11 16:05:44 -0800

    phase 1 - completing repl item

--- repl01.js
+++ repl01.js
@@ -1,65 +1,63 @@
-class origami {
-
-}
-
-const widget = (f) => {
-	if (f === undefined) {
-		this.folds = 1
-	} else { this.folds = f; }
-
-	this.Id = widget.nextCount();
-
-}
-
-widget.prototype.origami = () => {
-	console.log("Preparing to fold unit: " + this.Id)
-	this.foldAction();
-	if (this.folds === 2) {
-		console.log('makes an envelope');
+class Collection {
+	constructor(items = []) {
+		this.items = items;
 	}
 
-	if (this.folds == 3) {
-		console.log('makes a crane')
-	}
-
-	if (this.folds === 4) {
-		console.log(`makes a frog`)
-	}
-
-	if (this.folds === 5) {
-		console.log('makes an elephant')
-	}
-
-	if (6 == this.folds) { console.log('makes a crumpled wad') }
-}
-
-widget.prototype.foldAction = () => {
-	console.log('???')
-	for (i = 0; i < this.folds; i++) {
-
-		let step = 1 + i; console.log("fold... " + step)
-
+	count() {
+		return this.items.length
 	}
 }
 
-widget.nextCount = () => {
-	widget.count = widget.count || 0
-	widget.count += 1;
-	return widget.count
+class Origami {
+	constructor(folds = 1) {
+		this.folds = folds;
+		this.collect();
+	}
+
+	foldMe() {
+		console.log("Preparing to fold item...");
+		this.foldAction();
+		if (this.folds == 0) {
+			console.log("makes a piece of paper");
+		} else if (this.folds == 1) {
+			console.log("makes a folded sheet");
+		} else if (this.folds == 2) {
+			console.log('makes an envelope');
+		} else if (this.folds == 3) {
+			console.log('makes a crane');
+		} else if (this.folds == 4) {
+			console.log(`makes a frog`);
+		} else if (this.folds == 5) {
+			console.log('makes an elephant');
+		} else {
+			console.log('makes a crumpled wad');
+		}
+	}
+
+	foldAction() {
+		for (let i = 0; i < this.folds; i++) {
+			const step = 1 + i;
+			console.log("fold step... " + step);
+		}
+	}
+
+	collect() {
+		myCollection.items.push(self);
+	}
 }
 
-widget.getCount = () => {
-	return widget.count;
-}
+// IMPORTANT VARIABLE -- DO NOT REMOVE
+const myCollection = new Collection();
 
-const crane = new widget(3)
-const frog = new widget(4);
-const elephant = new widget(5);
-const oops = new widget(6);
+// Create items for my collection
+const crane = new Origami(3);
+const frog = new Origami(4);
+const elephant = new Origami(5);
+const oops = new Origami(6);
 
-console.log(widget.getCount())
+crane.foldMe();
+frog.foldMe();
+elephant.foldMe();
+oops.foldMe();
 
-crane.origami()
-frog.origami();
-elephant.origami();
-oops.origami()
+console.log("My collection has " + myCollection.count() + " items.");
